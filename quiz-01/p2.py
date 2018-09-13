bfc4f127ac39b9e6065a179b491b1c5e200ed836
Chloe <chloe.gottlieb30@myhunter.cuny.edu>
2018-09-13 09:08:19 -0400

    my terminal is being weird and when i vi into Readme.md it won't let me type at all, however when i'm not rouching the keyboard C is just being printed onto the screen with no end. I don't know why. Regardless, I'm putting what i did within my commit message bc it's very frustrating. I've tried making new files and vi into those. same issue. Anyway: touch p2.py, cat p1.py >> p2.py

--- quiz-01/p2.py
+++ quiz-01/p2.py
@@ -1,10 +1,7 @@
 
 def didit(name):
     response1 = name
-    response2 = "completed this problem"
+    response2 = " completed this problem"
     return response1 + response2
 
-print(didit(steve))
-
-#didn't get to part 3 of thequiz bc i got weird issues when i tried to run this: "print(didit(name))
-#NameError: name 'name' is not defined
+print(didit("steve"))
