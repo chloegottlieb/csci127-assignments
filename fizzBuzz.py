86c4edcc3365db5cc093ee36231b64d4c87de83c
Chloe <chloe.gottlieb30@myhunter.cuny.edu>
2018-09-27 06:53:44 -0400

    fizzbuzz

---
+++ fizzBuzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz(max_value):
+    i = 1
+    while i != max_value + 1:
+        if i % 5 == 0 and i % 3 == 0:
+            fizzBuzz = print('fizzBuzz')
+        elif i % 3 == 0:
+            print('fizz')
+        elif i % 5 == 0:
+            print('buzz')
+        else:
+            print(i)
+        i = i+1
+print(fizzbuzz(100))
