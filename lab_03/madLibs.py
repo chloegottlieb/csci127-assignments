85ec568cabc1eb15247b8a924ad0fd7c065bcee1
Gottlieb <gottliebc@IACNY-GOTLIC-LM.iac.corp>
2018-10-15 09:25:27 -0400

    added lab_03

---
+++ lab_03/madLibs.py
@@ -0,0 +1,8 @@
+import random
+EXCLAMATION=['Help', 'Ouch', 'Ahh', 'Yay']
+VERB=['fell', 'tripped', 'broke', 'hurt', 'pushed']
+BODY_PART=['arm', 'leg', 'head', 'finger', 'appendix']
+ADJ=['happy', 'brilliant', 'yellow', 'fun', 'amazing', 'handsome', 'slippery', 'rude']
+def mad_libs(l):
+    <EXCLAMATION>=rand.exclamation()
+    print("<EXCLAMATION> , I <VERB> down the stairs! I <VERB> my <BODY_PART> , the <ADJ> stairs were <ADJ>")
